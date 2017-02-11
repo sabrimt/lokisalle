995321517a4f8862f7c9f7a5e22ee851aefb9a7c
sabri <sabrimtir9@gmail.com>
2017-02-11 04:51:31 +0100

    suppression photos ?? bug photo actuelle

--- admin/gestion_salle.php
+++ admin/gestion_salle.php
@@ -142,12 +142,20 @@ if(isset($_POST['titre']) && isset($_POST['description']) && isset($_POST['pays'
 
                                 }else { // l'extension de la photo n'est pas valide
                                         $msg .= '<div class="erreur">L\'extension de la photo n\'est pas valide<br />Extensions acceptées: jpg / jpeg / png /gif</div>';
-                                }
+                                }   
                         }
+                        // suppression de photos (si checkbox coché)
+                                if(isset($_POST['ph_del'.$n]) && $_POST['ph_del'.$n] == "on")
+                                {
+                                    if(file_exists('../' . ${'photo_bdd'.$n})){
+                                        unlink('../' . ${'photo_bdd'.$n});
+                                    }
+                                    ${'photo_bdd'.$n} = "";
+                                }
                     }
 		}
                 // Vérification photos END
-			
+		// ENREGISTREMENT EN BDD	
 		if(empty($msg) && !empty($titre) && !empty($categorie) ) // s'il n'y a pas d'erreur au préalable, alors on lance l'enregistrement en BDD et champs requis remplis
 		{
 			
@@ -165,7 +173,7 @@ if(isset($_POST['titre']) && isset($_POST['description']) && isset($_POST['pays'
 		}
 	}
 	
-}
+}debug($_POST);
 
 include("../inc/header.inc.php");
 include("../inc/nav.inc.php");
@@ -275,13 +283,14 @@ include("../inc/nav.inc.php");
                                 {
                                     if($i > 1){
                                         $n = '_' . $i;
+                                        $prev =  '_' . ($i - 1);
                                     }else{
                                         $n = "";
+                                        $prev = "";
                                     }
                                     $i++;
                                 ?>
                                 <div class="form-group col-sm-4">
-
                                     <input type="file" class="input-file form-control" id="photo<?= $n ?>" name="photo<?= $n ?>"/>
                                     <label for="photo<?= $n ?>" id="photo_lab<?= $n ?>" class="btn btn-label btn-info"><?php echo ${'photo_action' . $n} ?>photo...<span class="glyphicon glyphicon-save"></span></label>
                                 <?php
@@ -291,9 +300,15 @@ include("../inc/nav.inc.php");
                                         <label for="photo_actuelle<?= $n ?>">Photo actuelle</label><br/>
                                         <img src="<?php echo URL . ${'photo' . $n} ?>" alt="<?php echo ${'photo' . $n} ?>" width="100%" />
                                         <input type="hidden" name="photo_actuelle<?= $n ?>" value="<?php echo ${'photo' . $n} ?>"/>
+                                        <div class="col-sm-9"><p><i>Supprimer cette photo ?</i></p></div>
+                                        <div class="col-sm-1 col-sm-offset-1"><input name="ph_del<?= $n ?>" type="checkbox"></div>
                                     </div>
                                 <?php
-                                }?>
+                                } elseif ($photo_exists xor $photo_exists_2 xor $photo_exists_3)
+                                {
+                                    echo 'brrrrraaaaaa';
+                                }
+                                ?>
                                 </div>
                                 <?php } ?><!-- fermeture foreach-->
 
