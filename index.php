55367058140f5831a641882d0effd0461145d169
Unknown <sabrimtir9@gmail.com>
2017-02-14 16:51:57 +0100

    bug fixed chargemet images à l'ajout

--- index.php
+++ index.php
@@ -197,7 +197,60 @@ include("inc/nav.inc.php");
         <div class="starter-template">
             <?php echo $msg; // variable initialisée dans le fichier init.inc.php ?>
         </div>
+          <div class="row">
+              <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                  <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                  <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                  <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                  <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+                </ol>
 
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+                  <div class="item active">
+                      <img src="img/homepub.jpg" alt="...">
+                      <div class="carousel-caption">
+                          <h2><b></b></h2>
+                        <p></p>
+                      </div>
+                  </div>
+                    <div class="item">
+                    <img src="img/conference_1.jpg" alt="...">
+                    <div class="carousel-caption">
+                        <h2><b>Salles Conférence</b></h2>
+                       <p>Conférence, Assemblée...</p>
+                    </div>
+                  </div>
+                  <div class="item">
+                    <img src="img/reu_1.jpg" alt="...">
+                    <div class="carousel-caption">
+                        <h2><b>Salles Réunion</b></h2>
+                       <p>Réunion, entretien, présentation...</p>
+                    </div>
+                  </div>
+                  <div class="item">
+                    <img src="img/standup2.jpg" alt="...">
+                    <div class="carousel-caption">
+                        <h2><b>Salles StandUp Meeting - Buffet</b></h2>
+                      <p>Pour Petit-Déjeuner, Rencontre, Débat...</p>
+                    </div>
+                  </div>
+                </div>
+
+                <!-- Controls -->
+                <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                  <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                  <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                  <span class="sr-only">Next</span>
+                </a>
+            </div>
+              
+          </div>
          <div class="row">
             <aside id="filtres-boutique" class="col-sm-3">
                     <form method="GET" action="" class="form">
@@ -257,57 +310,7 @@ include("inc/nav.inc.php");
                     </form>
              </aside>
              <div class="col-sm-9">
-             <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                <!-- Indicators -->
-                <ol class="carousel-indicators">
-                  <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                  <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                  <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                  <li data-target="#carousel-example-generic" data-slide-to="3"></li>
-                </ol>
-
-                <!-- Wrapper for slides -->
-                <div class="carousel-inner" role="listbox">
-                  <div class="item active">
-                      <img src="img/homepub.jpg" alt="...">
-                      <div class="carousel-caption">
-                          <h2><b></b></h2>
-                        <p></p>
-                      </div>
-                  </div>
-                    <div class="item">
-                    <img src="img/conference_1.jpg" alt="...">
-                    <div class="carousel-caption">
-                        <h2><b>Salles Conférence</b></h2>
-                       <p>Conférence, Assemblée...</p>
-                    </div>
-                  </div>
-                  <div class="item">
-                    <img src="img/reu_1.jpg" alt="...">
-                    <div class="carousel-caption">
-                        <h2><b>Salles Réunion</b></h2>
-                       <p>Réunion, entretien, présentation...</p>
-                    </div>
-                  </div>
-                  <div class="item">
-                    <img src="img/standup2.jpg" alt="...">
-                    <div class="carousel-caption">
-                        <h2><b>Salles StandUp Meeting - Buffet</b></h2>
-                      <p>Pour Petit-Déjeuner, Rencontre, Débat...</p>
-                    </div>
-                  </div>
-                </div>
-
-                <!-- Controls -->
-                <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-                  <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                  <span class="sr-only">Previous</span>
-                </a>
-                <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-                  <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                  <span class="sr-only">Next</span>
-                </a>
-            </div>
+             
                  <div class="nb-produits"><h4><?= $nb_produit_texte ?></h4></div>
               <?php 
                     // Affichage des différents produits
